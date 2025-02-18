5004ab54d954dd444af01c7cb57543442024ed24
Lana Ramos <lanadefy@gmail.com>
2025-02-18 15:11:22 -0300

    Adicionando as bibliotecas

---
+++ Conversor-analogico-digital-EmbarcaTech.c
@@ -0,0 +1,16 @@
+// Bibliotecas necessárias para o funcionamento do programa
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+
+
+int main()
+{
+    stdio_init_all();
+
+    return 0;
+}
